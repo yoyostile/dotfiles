5e22c1e13a7d594868cda902eb6579898481fed1
Johannes Heck <yoyostile@gmail.com>
2021-01-07 14:45:12 +0100

    I did things I cannot remember

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,12 +4,12 @@ cd "$(dirname "${BASH_SOURCE}")";
 
 git pull origin main;
 
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
-brew install python;
-sudo pip3 install pywal;
-brew bundle;
-bash ./.macos;
+#/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
+#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+#brew install python;
+#sudo pip3 install pywal;
+#brew bundle;
+#bash ./.macos;
 
 function doIt() {
   rsync --exclude ".git/" \
